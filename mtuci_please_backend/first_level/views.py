e5a3ec3ac74f2e86efb4046fad647987f826b6c9
M1NT <ilya.ablyazov.2004@gmail.com>
2023-11-27 18:15:27 +0300

    Work with students

--- mtuci_please_backend/first_level/views.py
+++ mtuci_please_backend/first_level/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from random import *
 from .models import Person
+from datetime import *
 
 
 class PersonsRandomize(APIView):
@@ -10,21 +11,38 @@ class PersonsRandomize(APIView):
         for i in range(10):
             gender = choice(['М', 'Ж'])
             person = Person.objects.filter(gender=gender)
-            photo = choice(person.values_list('photo'))
+            photo = choice(person.values_list('photo'))[0]
             surname = choice(person.values_list('surname'))
             name = choice(person.values_list('name'))
             middle_name = choice(person.values_list('middle_name'))
             full_name = surname[0] + " " + name[0] + " " + middle_name[0]
-            output[i] = [photo, full_name]
+            output[i] = [photo, full_name, dict()]
 
             if randint(1, 10) <= 6:
-                pass_photo = photo
                 if randint(1, 10) <= 5:
-                    pass
+                    wrong_pass_photo = photo = choice(person.values_list('photo'))[0]
+                    while wrong_pass_photo == photo:
+                        photo = choice(person.values_list('photo'))[0]
+                    output[i][2]["pass"] = wrong_pass_photo
+                    output[i].append(1)
+                else:
+                    year = 2003
+                    output[i][2]["pass"] = photo, full_name, year
             else:
                 if randint(1, 10) <= 6:
-                    student_card_photo = photo
+                    if len(output[i]) == 3:
+                        if randint(1, 10) <= 5:
+                            if randint(1, 2) == 1:
+                                pass
+                            else:
+                                pass
+                    else:
+                        output[i][2]["student_it"] = photo, t
 
                 if randint(1, 10) <= 6:
                     passport_photo = photo
+
+            if len(output[i]) == 3:
+                output[i].append(0)
+
         return Response(output)
