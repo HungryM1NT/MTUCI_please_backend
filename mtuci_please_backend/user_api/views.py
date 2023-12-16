c141c2fb2c5a2708e2f6d85526181018af53a9d7
m1nt <ilya.ablyazov.2004@gmail.com>
2023-12-16 15:56:21 +0300

    trying to update userview 3

--- mtuci_please_backend/user_api/views.py
+++ mtuci_please_backend/user_api/views.py
@@ -54,7 +54,7 @@ class UserView(APIView):
     permission_classes = (permissions.AllowAny,)
     authentication_classes = ()
     def get(self, request):
-        if request.user.is_authenticated:
+        if request.user:
             serializer = UserSerializer(request.user)
             return Response({'user': serializer.data}, status=status.HTTP_200_OK)
         return Response(status=status.HTTP_200_OK)
